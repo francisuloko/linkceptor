940069c8be136b8ea333420343a80d6f7eaf2c66
Francis Uloko <francisuloko@yahoo.com>
2023-05-29 01:37:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 <script>
-// Function to intercept link clicks and open them in an iframe
-function interceptLinks() {
-  var links = document.getElementsByTagName('a'); // Get all anchor tags
+  // Wait for the document to fully load
+  document.addEventListener('DOMContentLoaded', function() {
+    // Get all the links on the page
+    var links = document.getElementsByTagName('a');
 
-  for (var i = 0; i < links.length; i++) {
-    var link = links[i];
+    // Loop through each link
+    for (var i = 0; i < links.length; i++) {
+      var link = links[i];
 
-    // Check if the link is an external or ref link
-    if (link.hostname !== window.location.hostname || link.getAttribute('target') === '_blank') {
-      // Intercept the click event
-      link.addEventListener('click', function(event) {
-        event.preventDefault(); // Prevent the default link behavior
+      // Check if the link is an external or referral link
+      if (link.hostname !== window.location.hostname) {
+        // Intercept the click event
+        link.addEventListener('click', function(event) {
+          event.preventDefault(); // Prevent the link from opening normally
 
-        // Create an iframe element
-        var iframe = document.createElement('iframe');
-        iframe.src = this.href; // Set the iframe source to the link URL
-        iframe.style.width = '100%';
-        iframe.style.height = '100vh';
-
-        // Replace the current page content with the iframe
-        document.body.innerHTML = '';
-        document.body.appendChild(iframe);
-      });
+          // Create an iframe element
+          var iframe = document.createElement('iframe');
+          iframe.src = this.href;
+          iframe.style.width = '100%';
+          iframe.style.height = '100vh';
+          iframe.style.border = 'none';
+          iframe.setAttribute('sandbox', 'allow-same-origin allow-scripts');
+          
+          // Replace the link with the iframe
+          this.parentNode.replaceChild(iframe, this);
+        });
+      }
     }
-  }
-}
-
-// Call the interceptLinks function when the document has finished loading
-document.addEventListener('DOMContentLoaded', interceptLinks);
+  });
 </script>
